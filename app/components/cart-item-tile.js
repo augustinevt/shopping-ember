e1bb54f897f58de72ca3d010afe1432b29243ec7
August von Trapp and Wolfgang Warneke <august+wolfgang@>
2016-08-24 16:48:19 -0700

    cart total almost there.....

--- app/components/cart-item-tile.js
+++ app/components/cart-item-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  multiItemCost: Ember.computed('product.cost', 'qty', function() {
+    return this.get('product.cost') * this.get('qty');
+  })
 });
