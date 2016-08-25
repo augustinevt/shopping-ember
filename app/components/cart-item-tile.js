a6ec4be5aa971ccaffbcefd2f4f3e20bbc24b3c5
August von Trapp and Wolfgang Warneke <august+wolfgang@>
2016-08-25 16:49:36 -0700

    fixed shopping cart bug

--- app/components/cart-item-tile.js
+++ app/components/cart-item-tile.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
   multiItemCost: Ember.computed('product.cost', 'qty', function() {
     return this.get('product.cost') * this.get('qty');
-  })
+  }),
+  actions: {
+    removeItem(product){
+      // console.log(product);
+      this.get('shoppingCart').remove(product);
+
+    }
+  }
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  totalItems: 0,
   cartTotal: Ember.computed('items', function() {
     var items = this.get('items');
     var total = 0;
@@ -14,6 +15,7 @@ export default Ember.Service.extend({
   }),
   add(product) {
     var items = this.get('items');
+    this.set('totalItems', (this.get('totalItems') + 1));
     var alreadyAdded = false;
     items.forEach((item) => {
       if(item.product.id === product.id){
@@ -25,5 +27,25 @@ export default Ember.Service.extend({
     if(!alreadyAdded){
       items.pushObject({"product": product, "qty": 1});
     }
+  },
+  remove(product){
+    var items = this.get('items');
+    var itemToRemove;
+    // var counter = 0;
+
+    for(var item of items) {
+      if(item.product.id === product.id ){
+        var qty = item.qty;
+        this.set('totalItems', (this.get('totalItems') - qty));
+        itemToRemove = item;
+      }
+      // counter++;
+      // console.log(this.get('items['+counter+']'));
+    }
+    var updated = this.get('items').slice();
+    updated.removeObject(itemToRemove);
+    this.set('items', updated);
+    // console.log(this.get('items'));
+    // this.get('items.['+counter+']').destroy();
   }
 });
