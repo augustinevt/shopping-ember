271d900d82bb7e87c41847e31401700792e8ecd3
August von Trapp and Wolfgang Warneke <august+wolfgang@>
2016-08-25 08:28:04 -0700

    refactor shopping cart total into shopping cart service instead of on component

--- app/components/display-shopping-cart.js
+++ app/components/display-shopping-cart.js
@@ -2,17 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
-  cartTotal: Ember.computed('shoppingCart', function() {
-    var items = this.get('shoppingCart.items');
-    console.log(items);
-    var total = 0;
-    var counter = 0;
-    items.forEach((item) => {
-      console.log("QTY " + item.qty);
-      console.log(item.product.get('cost'));
-      total += item.qty * item.product.get('cost');
-      counter ++;
-    });
-    return total;
-  })
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
-
+  cartTotal: Ember.computed('items', function() {
+    var items = this.get('items');
+    var total = 0;
+    var counter = 0;
+    items.forEach((item) => {
+      total += item.qty * item.product.get('cost');
+      counter ++;
+    });
+    return total;
+  }),
   add(product) {
     var items = this.get('items');
     var alreadyAdded = false;
